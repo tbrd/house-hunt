99bac230e70ea46e6bea01d59b3c392bb7f106dd
TOM HAMSHERE <tom.hamshere@gmail.com>
2019-06-11 05:53:36 +0100

    First commit

---
+++ house-hunt-api/routes/house.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+/* GET house data. */
+const house = function(req, res, next) {
+  // TODO lookup address by postcode
+  // TODO /house should return address
+  // TODO /house should return JSON
+  res.send('info');
+};
+
+module.exports = house;
