88e1a6e21c56d4ffbaf5ac9faf5f2ae30a4cd7db
TOM HAMSHERE <tom.hamshere@gmail.com>
2019-06-12 08:54:13 +0100

    Add Location stuff

--- house-hunt-api/routes/house.js
+++ house-hunt-api/routes/house.js
@@ -1,11 +1,38 @@
 var express = require('express');
 
+const googleMapsClient = require('@google/maps').createClient({
+  key: 'AIzaSyBNbhozbUURNFlC4F0unsfR30FrxpwU18c',
+  Promise: Promise
+});
+
+const getDrivingDirections = (origin, destination) => googleMapsClient.directions({origin, destination}).asPromise();
+const getGeocode = (postcode) => googleMapsClient.geocode({address: postcode, region: 'UK'}).asPromise();
+const getDrivingDirectionsToRuth = postcode => getDrivingDirections(postcode, "N19 4JN");
+const getDrivingDirectionsToTheField = postcode => getDrivingDirections(postcode, "M468+8X Stokenchurch");
+
 /* GET house data. */
 const house = function(req, res, next) {
-  // TODO lookup address by postcode
-  // TODO /house should return address
-  // TODO /house should return JSON
-  res.send('info');
+  const postcode = req.query.postcode;
+
+  const promises = [
+    getGeocode,
+    getDrivingDirectionsToRuth,
+    getDrivingDirectionsToTheField
+  ].map(method => method(postcode))
+
+  Promise.all(promises)
+    .then((results) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.send({
+        geocode: results[0],
+        drivingDirectionsToRuth: results[1],
+        drivingDirectionsToTheField: results[2]
+      });
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+  
 };
 
 module.exports = house;
